b2b9417e4440566d9eda916b3e9eee88d14739dc
AkshaySahu05 <akshay.sahu@rungta.org>
2025-12-29 21:56:50 +0530

    new practicals

---
+++ practical 10/row_wise_sum_of_2D_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int r, c, i, j;
+    int a[10][10];
+    int sum;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter elements of 2D array:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < r; i++) {
+        sum = 0;
+        for(j = 0; j < c; j++) {
+            sum = sum + a[i][j];
+        }
+        printf("Sum of row %d = %d\n", i + 1, sum);
+    }
+
+    return 0;
+}
